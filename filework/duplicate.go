d1d72a3e20046cdbe117890ac6f36a5330e0ff6a
White-AK111 <formatmyc@gmail.com>
2021-10-26 02:26:25 -0700

    clone

---
+++ filework/duplicate.go
@@ -0,0 +1,93 @@
+package filework
+
+import (
+	"fmt"
+	"github.com/White-AK111/GB_Go_Level2/Lesson8/config"
+	"os"
+	"sort"
+	"strings"
+)
+
+// DoDuplicateFiles function for find duplicate file in source directory files compares by hash, optionality user can delete all duplicate files, return error
+func DoDuplicateFiles(cfg *config.Config) error {
+	fInfo := filesInfo{}
+	fInfo.directoryList = append(fInfo.directoryList, cfg.App.SourcePath)
+
+	err := findAllFiles(cfg, &fInfo)
+	if err != nil {
+		cfg.App.ErrorLogger.Printf("error on find all files in source path: %s", err)
+		return err
+	}
+
+	// sort files, files in root directory priority are considered like original
+	sort.Slice(fInfo.allFilesList, func(i, j int) bool {
+		return fInfo.allFilesList[i].Path < fInfo.allFilesList[j].Path
+	})
+
+	// compare files
+	for i, file := range fInfo.allFilesList {
+		if file.contains(fInfo.allFilesList, i) {
+			fmt.Printf("Duplicate file: %s	Original file: %s\n", file.Path, file.OriginalFile.Path)
+			fInfo.duplicateFilesList = append(fInfo.duplicateFilesList, file)
+		}
+	}
+
+	fmt.Printf("Total files: %d\n", len(fInfo.allFilesList))
+	fmt.Printf("Duplicate files (without original file): %d\n", len(fInfo.duplicateFilesList))
+
+	// delete files if get a flag, get approval from user
+	if cfg.App.FlagDelete || cfg.App.RunInTest {
+		if len(fInfo.duplicateFilesList) == 0 {
+			fmt.Println("No files for delete!")
+		} else {
+			var confirm string
+			if !cfg.App.RunInTest {
+				for strings.ToUpper(confirm) != "Y" && strings.ToUpper(confirm) != "N" {
+					fmt.Print("Delete this duplicate files? (Y/N): ")
+					_, err = fmt.Fscan(os.Stdin, &confirm)
+					if err != nil {
+						cfg.App.ErrorLogger.Printf("error on get approval from console: %s\n", err)
+						return err
+					}
+				}
+			}
+			if strings.ToUpper(confirm) == "Y" || cfg.App.RunInTest {
+				err = deleteFiles(cfg, &fInfo)
+				if err != nil {
+					cfg.App.ErrorLogger.Printf("error on delete files: %s\n", err)
+					return err
+				}
+				fmt.Println("Files deleted!")
+			}
+		}
+	}
+
+	return nil
+}
+
+// deleteFiles function delete files, return error
+func deleteFiles(cfg *config.Config, fInfo *filesInfo) error {
+	wp := newWorkerPool(cfg.App.CountGoroutine)
+	defer wp.wg.Wait()
+
+	for _, file := range fInfo.duplicateFilesList {
+		wp.wg.Add(1)
+		go func(file FileEntity) {
+			defer func() {
+				wp.mu.Unlock()
+				// read to release a slot
+				<-wp.semaphoreChan
+				fInfo.deleteFilesList = append(fInfo.deleteFilesList, file)
+				wp.wg.Done()
+			}()
+			// block while full
+			wp.semaphoreChan <- struct{}{}
+			wp.mu.Lock()
+			if err := os.Remove(file.Path); err != nil {
+				cfg.App.ErrorLogger.Printf("error on delete file %s: %s\n", file.Path, err)
+			}
+		}(file)
+	}
+
+	return nil
+}
---
+++ filework/filework_test.go
@@ -0,0 +1,186 @@
+package filework
+
+import (
+	"github.com/White-AK111/GB_Go_Level2/Lesson8/config"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"log"
+	"testing"
+)
+
+// TestDoDuplicateFiles test for DoDuplicateFiles function
+func TestDoDuplicateFiles(t *testing.T) {
+	cfg, err := config.Init()
+	if err != nil {
+		t.Fatalf("error on load configuration file: %s", err)
+	}
+
+	cfg.App.FlagDelete = true
+	cfg.App.SourcePath = "../TestFiles"
+	cfg.App.RunInTest = true
+
+	filesB, _ := ioutil.ReadDir(cfg.App.SourcePath)
+
+	err = DoDuplicateFiles(cfg)
+	if err != nil {
+		t.Fatalf("error on duplicate files function: %s", err)
+	}
+
+	filesA, _ := ioutil.ReadDir(cfg.App.SourcePath)
+
+	assert.NotEqual(t, filesB, filesA, "count of file don't changes, before: %d, after: %d", len(filesB), len(filesA))
+}
+
+// TestDoRandomCopyFiles test for DoRandomCopyFiles function
+func TestDoRandomCopyFiles(t *testing.T) {
+	cfg, err := config.Init()
+	if err != nil {
+		t.Fatalf("error on load configuration file: %s", err)
+	}
+
+	cfg.App.FlagRandCopy = true
+	cfg.App.FlagDelete = false
+	cfg.App.SourcePath = "../TestFiles"
+	cfg.App.RunInTest = true
+
+	filesB, _ := ioutil.ReadDir(cfg.App.SourcePath)
+
+	err = DoRandomCopyFiles(cfg)
+	if err != nil {
+		t.Fatalf("error on random copy files function: %s", err)
+	}
+
+	filesA, _ := ioutil.ReadDir(cfg.App.SourcePath)
+
+	assert.NotEqual(t, filesB, filesA, "count of file don't changes, before: %d, after: %d", len(filesB), len(filesA))
+}
+
+// BenchmarkDoDuplicateFiles_1go bench for DoDuplicateFiles function, use 1 goroutine
+func BenchmarkDoDuplicateFiles_1go(b *testing.B) {
+	cfg, err := config.Init()
+	if err != nil {
+		b.Fatalf("error on load configuration file: %s", err)
+	}
+
+	cfg.App.FlagDelete = false
+	cfg.App.SourcePath = "../TestFiles"
+	cfg.App.RunInTest = true
+	cfg.App.CountGoroutine = 1
+
+	for i := 0; i < b.N; i++ {
+		if err := DoDuplicateFiles(cfg); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// BenchmarkDoRandomCopyFiles_1go bench for DoRandomCopyFiles function, use 1 goroutine
+func BenchmarkDoRandomCopyFiles_1go(b *testing.B) {
+	cfg, err := config.Init()
+	if err != nil {
+		b.Fatalf("error on load configuration file: %s", err)
+	}
+
+	cfg.App.FlagRandCopy = true
+	cfg.App.FlagDelete = false
+	cfg.App.SourcePath = "../TestFiles"
+	cfg.App.RunInTest = true
+	cfg.App.CountGoroutine = 1
+	cfg.App.CountRndCopyIter = 1000
+
+	for i := 0; i < b.N; i++ {
+		if err := DoRandomCopyFiles(cfg); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// BenchmarkDoDuplicateFiles_1000go bench for DoDuplicateFiles function, use 1000 goroutine
+func BenchmarkDoDuplicateFiles_1000go(b *testing.B) {
+	cfg, err := config.Init()
+	if err != nil {
+		b.Fatalf("error on load configuration file: %s", err)
+	}
+
+	cfg.App.FlagDelete = false
+	cfg.App.SourcePath = "../TestFiles"
+	cfg.App.RunInTest = true
+	cfg.App.CountGoroutine = 1000
+
+	for i := 0; i < b.N; i++ {
+		if err := DoDuplicateFiles(cfg); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// BenchmarkDoRandomCopyFiles_1000go bench for DoRandomCopyFiles function, use 1000 goroutine
+func BenchmarkDoRandomCopyFiles_1000go(b *testing.B) {
+	cfg, err := config.Init()
+	if err != nil {
+		b.Fatalf("error on load configuration file: %s", err)
+	}
+
+	cfg.App.FlagRandCopy = true
+	cfg.App.FlagDelete = false
+	cfg.App.SourcePath = "../TestFiles"
+	cfg.App.RunInTest = true
+	cfg.App.CountGoroutine = 1000
+	cfg.App.CountRndCopyIter = 1000
+
+	for i := 0; i < b.N; i++ {
+		if err := DoRandomCopyFiles(cfg); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// ExampleDoDuplicateFiles example for use DoDuplicateFiles function
+func ExampleDoDuplicateFiles() {
+	// set source directory in config.yaml file or use flags (-h for help)
+	// ...
+
+	// init configuration for app
+	cfg, err := config.Init()
+	if err != nil {
+		log.Fatalf("error on load configration file: %s", err)
+	}
+
+	// init flags for app
+	err = cfg.InitFlags()
+	if err != nil {
+		log.Fatalf("error on initialize flags: %s", err)
+	}
+
+	// function delete find duplicate files in source directory and delete this if flag -rm use (or it's set in config file config.yaml)
+	err = DoDuplicateFiles(cfg)
+	if err != nil {
+		cfg.App.ErrorLogger.Fatalf("Error on duplicate files function: %s", err)
+	}
+}
+
+// ExampleDoRandomCopyFiles example for use DoRandomCopyFiles function
+func ExampleDoRandomCopyFiles() {
+	// set source directory in config.yaml file or use flags (-h for help)
+	// ...
+
+	// init configuration for app
+	cfg, err := config.Init()
+	if err != nil {
+		log.Fatalf("error on load configration file: %s", err)
+	}
+
+	// init flags for app
+	err = cfg.InitFlags()
+	if err != nil {
+		log.Fatalf("error on initialize flags: %s", err)
+	}
+
+	// function delete random create files in source directory use flag -cp (or it's set in config file config.yaml)
+	if cfg.App.FlagRandCopy {
+		err = DoRandomCopyFiles(cfg)
+		if err != nil {
+			cfg.App.ErrorLogger.Fatalf("Error on random copy files function: %s", err)
+		}
+	}
+}
